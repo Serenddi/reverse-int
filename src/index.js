075424c109167ac2a8caec8e072b23173d1dfde1
Anastasiia Mikhaleva <dadanastya24342@gmail.com>
2022-02-16 22:38:50 +0700

    feat: do index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+   if (n > 0) {
+     let a = [n]
+     let b = a.toString().split('');
+     let c = b.reverse();
+     let d = c.join('');
+    return d;
+ }
+ else if (n < 0) {
+    let a = [n * -1]
+    let b = a.toString().split('');
+    let c = b.reverse();
+    let d = c.join('');
+   return d;
+ }
 }
